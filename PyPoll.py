bb9b657090d0b3345053d0e8b74f6bdc355c69c3
Shea T Walker <madmax_113@hotmail.com>
2021-11-11 22:33:56 -0500

    Adding the election analysis.

--- PyPoll.py
+++ PyPoll.py
@@ -74,11 +74,10 @@ with open(file_to_save, "w") as txt_file:
     # Print the winning candidates' results to the terminal.
     winning_candidate_summary = (
         f"-------------------------\n"
-        f"Winner: {winning_candidate}\n"
+        f"Winner: {winning_candidate}\n"        
         f"Winning Vote Count: {winning_count:,}\n"
         f"Winning Percentage: {winning_percentage:.1f}%\n"
         f"-------------------------\n")
 
     print(winning_candidate_summary)
     txt_file.write(winning_candidate_summary)
-    
